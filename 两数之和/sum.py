02f8bec84c55de542f56065ddc37d591b8a609d7
helloqingfeng <tw19941203541@163.com>
2018-05-07 15:55:53 +0800

    两数之和

---
+++ 两数之和/sum.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dict = {}
+        for index, value in enumerate(nums):
+            if target - value in dict:
+                return [dict[target - value], index]
+            dict[value] = index
