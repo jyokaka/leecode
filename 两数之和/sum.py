b92f1fa7c0fff7ce0741accacb5c1929017787dc
helloqingfeng <tw19941203541@163.com>
2018-05-07 16:43:02 +0800

    两数之和

--- 两数之和/sum.py
+++ 两数之和/sum.py
@@ -8,8 +8,8 @@ class Solution:
         :type target: int
         :rtype: List[int]
         """
-        dict = {}
-        for index, value in enumerate(nums):
-            if target - value in dict:
-                return [dict[target - value], index]
-            dict[value] = index
+        dict = {}   
+        for index, value in enumerate(nums):    #对一个列表，既要遍历索引又要遍历元素时,可以利用enumerate实现
+            if target - value in dict:          #判断target-value是否在dict中存在，不存在的话，将他加入字典中，也就是最后一句
+                return [dict[target - value], index]  
+            dict[value] = index    #将键值对加入字典中中
